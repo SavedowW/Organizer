61dc201bb4bf762772c8ba5e2000a72327e91257
SavedowW <velikanov-em@yandex.ru>
2022-11-13 19:49:58 +0300

    DB class

--- SessionControl.php
+++ SessionControl.php
@@ -9,31 +9,4 @@ function checkSession()
     }
     $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp    
 }
-
-function getLogPass(mysqli &$mysqli, string $login, string $password)
-{
-    $req = $mysqli->prepare("SELECT ID, Login, Password, Name, LastName FROM user WHERE login=? AND password=?;");
-
-    $req->bind_param("ss", $login, $password);
-    $req->execute();
-    return $req->get_result();
-}
-
-function countUsers(mysqli &$mysqli, string $login)
-{
-    $req = $mysqli->prepare("SELECT count(ID) FROM user WHERE login=?;");
-
-    $req->bind_param("s", $login);
-    $req->execute();
-    return (int)$req->get_result()->fetch_row()[0];
-}
-
-function addUser(mysqli &$mysqli, string $nLogin, string $nPassword,
-    string $nName, string $nLastName)
-{
-    $req = $mysqli->prepare("Insert into `user` (Login, Password, Name, LastName, RightsID) values (?, ?, ?, ?, 1);");
-
-    $req->bind_param("ssss", $nLogin, $nPassword, $nName, $nLastName);
-    $req->execute();
-}
 ?>
