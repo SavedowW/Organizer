c13faa8c726f7dfdd7fa08032ca62573d43b6968
SavedowW <velikanov-em@yandex.ru>
2022-11-28 10:39:11 +0300

    implemented task deleting

--- bdconf.sql
+++ bdconf.sql
@@ -40,6 +40,6 @@ Insert into `user` (Login, Password, RightsID)
   values ("admin@gmail.com", "admin", 2);
 
 INSERT INTO `task` (UserID, Name, Description, CreationDate, Deadline, Priority, StartDate) VALUES
-  (1, "Ride wife", "Life good", CURDATE(), "2022-11-15", 50, "2022-11-14"),
-  (1, "Wife fight back", "Kill wife", CURDATE(), "2022-11-16", 60, "2022-11-15"),
-  (1, "Think about wife", "Regret", CURDATE(), "2022-11-17", 70, "2022-11-16");
+  (1, "Ride wife", "Life good", CURDATE(), "2022-11-15", 2, "2022-11-14"),
+  (1, "Wife fight back", "Kill wife", CURDATE(), "2022-11-16", 3, "2022-11-15"),
+  (1, "Think about wife", "Regret", CURDATE(), "2022-11-17", 1, "2022-11-16");
